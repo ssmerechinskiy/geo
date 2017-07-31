c4d5f3ea737bf800c4f6f7531d91aab40f7f5cc0
serhii smerechynskyi <serhii.smerechynskyi@mantu.im>
2017-07-31 22:01:02 +0300

    add current location with geofence displaying

--- app/src/main/java/com/sergey/geo/MapsActivity.java
+++ app/src/main/java/com/sergey/geo/MapsActivity.java
@@ -1,27 +1,66 @@
 package com.sergey.geo;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+    private final static String TAG = MapsActivity.class.getSimpleName();
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private String mprovider;
+
+    private Location currentLocation = null;
+    private Marker currentLocationMarker = null;
+
+    private boolean permissionsGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+        updateCurrentLocation();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(locationManager != null) {
+            locationManager.removeUpdates(locationListener);
+        }
     }
 
 
@@ -36,11 +75,79 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady");
         mMap = googleMap;
+        mMap.setOnMapLongClickListener(this);
+        displayCurrentLocation();
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    private void displayCurrentLocation() {
+        if (currentLocation != null)
+            locationListener.onLocationChanged(currentLocation);
+        else
+            Toast.makeText(getBaseContext(), "No Location Provider Found Check Your Code", Toast.LENGTH_SHORT).show();
+    }
+
+    private void updateCurrentLocation() {
+        Criteria criteria = new Criteria();
+        mprovider = locationManager.getBestProvider(criteria, false);
+        if (mprovider != null && !mprovider.equals("")) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            permissionsGranted = true;
+            currentLocation = locationManager.getLastKnownLocation(mprovider);
+            locationManager.requestLocationUpdates(mprovider, 15000, 1, locationListener);
+        }
+    }
+
+    private  LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Toast.makeText(getBaseContext(), "Location has changed", Toast.LENGTH_SHORT).show();
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            LatLng myGeoPoint = new LatLng(latitude, longitude);
+
+            currentLocation = location;
+
+            if(currentLocationMarker != null) {
+                currentLocationMarker.remove();
+            }
+
+            currentLocationMarker = mMap.addMarker(new MarkerOptions().position(myGeoPoint).title("Your current location"));
+            currentLocationMarker.showInfoWindow();
+//            mMap.moveCamera(CameraUpdateFactory.newLatLng(myGeoPoint));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 18.0f));
+
+            CircleOptions circleOptions = new CircleOptions()
+                    .center( new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()) )
+                    .radius(20)
+                    .fillColor(0x40ff0000)
+                    .strokeColor(Color.TRANSPARENT)
+                    .strokeWidth(2);
+            Circle circle = mMap.addCircle(circleOptions);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
     }
 }
